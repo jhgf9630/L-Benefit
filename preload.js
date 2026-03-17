df997a890639d3aabfba210eab3a53ae1e4692cb
Seung-Ju Wi <108789149+jhgf9630@users.noreply.github.com>
2026-03-17 15:44:36 +0900

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,8 +1,10 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld("syncAPI", {
-  getStatus:    () => ipcRenderer.invoke("get-sync-status"),
-  getDataPath:  () => ipcRenderer.invoke("get-userdata-path"),
-  readBundleData: () => ipcRenderer.invoke("read-bundle-data"),
-  onSyncComplete: (callback) => ipcRenderer.on("sync-complete", (_event, status) => callback(status))
+  getStatus:       () => ipcRenderer.invoke("get-sync-status"),
+  getDataPath:     () => ipcRenderer.invoke("get-userdata-path"),
+  readBundleData:  () => ipcRenderer.invoke("read-bundle-data"),
+  requestSlmSync:  () => ipcRenderer.invoke("request-slm-sync"),
+  onSyncComplete:  (cb) => ipcRenderer.on("sync-complete",      (_e, s) => cb(s)),
+  onSyncStatus:    (cb) => ipcRenderer.on("sync-status-update", (_e, s) => cb(s))
 });
